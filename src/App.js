e7073da5944cb38bebd4bb0d26033c2fa520d457
melika760 <melikaaatafazoli@gmail.com>
2024-11-23 14:30:03 +0000

    add react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
+
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
   );
 }
 
