7316cab775bdf9c511adbf54db6da3cab4dcfd0d
melika760 <melikaaatafazoli@gmail.com>
2024-11-28 14:21:06 +0000

    add more data in destination page

--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import Nav from '../Components/Nav'
-
+import moon from "../assets/destination/image-moon.png"
 const Destination = () => {
   return (
-    <section className="xl:bg-[url('./assets/destination/background-destination-desktop.jpg')] md:bg-[url('./assets/home/background-home-tablet.jpg')] bg-cover bg-center h-screen overflow-hidden">
+<section className="xl:bg-[url('./assets/destination/background-destination-desktop.jpg')] md:bg-[url('./assets/home/background-home-tablet.jpg')] bg-cover bg-center bg-repeat-y h-[200vh]">
+
         <Nav/>
+        <div className='w-[1110px] h-[792px] m-auto pt-12 mt-4'>
+        <h2 className='uppercase text-white text-3xl'>  <span className='opacity-20 '>01</span>  Pick Your destination</h2>
+        <div className='flex space-x-4 mt-[24px]'>
+<div className=''>
+    <img src={moon} alt='moon' className='w-[500px] h-[500px]'/>
+</div>
+        </div>
+        </div>
+        
     </section>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Nav from '../Components/Nav'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
@@ -9,7 +10,7 @@ const Home = () => {
         <div className=' text-white w-[540px] md:w-[512px] h-[343px] grid grid-cols-1 gap-5'><p className='uppercase font-light text-blue-200 leading-none'>So you want to travel to </p>
         <h2 className='font-light text-9xl leading-none'>Space</h2>
         <p className=' font-light text-blue-200 leading-6'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience! </p></div>
-        <button className='w-[272px] h-[272px] bg-white rounded-full text-4xl font-light'>Explore</button>
+        <button className='w-[272px] h-[272px] bg-white rounded-full text-4xl font-light'><Link to={"/destination"}>Explore</Link></button>
       </div>
     </section>
   )
