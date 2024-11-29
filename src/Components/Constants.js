c6615a9e2af91769adff89bca5f3f3b0bd7dae73
melika760 <melikaaatafazoli@gmail.com>
2024-11-29 17:52:32 +0000

    Update Constants.js

--- src/Components/Constants.js
+++ src/Components/Constants.js
@@ -4,6 +4,8 @@ import Europa from "../assets/destination/image-europa.png"
 import Titan from "../assets/destination/image-titan.png"
 import Mark from "../assets/crew/image-mark-shuttleworth.png"
 import Douglas from "../assets/crew/image-douglas-hurley.png"
+import Victor from "../assets/crew/image-victor-glover.png"
+import Anousheh from "../assets/crew/image-anousheh-ansari.png"
 export const datas=[
     {img:moon,
 Title:"Moon",
@@ -41,5 +43,19 @@ export const crewMembers=[
         Name:"MARK SHUTTLEWORTH",
         job:"Mission Specialist",
         description:"Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist."
+    },
+    {
+        img:Victor,
+        Title:"Victor",
+        Name:"Victor Glover",
+        job:"PILOT",
+        description:"Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer. "
+    },
+    {
+        img:Anousheh,
+        Title:"Anousheh",
+        Name:"Anousheh Ansari",
+        job:"PILOT",
+        description:"Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space. "
     }
 ]
