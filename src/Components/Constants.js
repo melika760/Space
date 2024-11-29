f36075459dc0f7ac4626a209b17ad36db8eec483
melika760 <melikaaatafazoli@gmail.com>
2024-11-29 18:35:39 +0000

    make it responsive for tablets

--- src/Components/Constants.js
+++ src/Components/Constants.js
@@ -55,7 +55,7 @@ export const crewMembers=[
         img:Anousheh,
         Title:"Anousheh",
         Name:"Anousheh Ansari",
-        job:"PILOT",
+        job:"Flight Engineer",
         description:"Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space. "
     }
 ]
--- src/pages/Crew.js
+++ src/pages/Crew.js
@@ -9,22 +9,22 @@ const Crew = () => {
     }
     const activeData=crewMembers.find((data)=>data.Title===active)
   return (
-<PageLayout bgClass={"xl:bg-[url('./assets/crew/background-crew-desktop.jpg')] md:bg-[url('./assets/crew/background-crew-tablet.jpg')] bg-repeat-y"}>
+<PageLayout bgClass={"xl:bg-[url('./assets/crew/background-crew-desktop.jpg')] md:bg-[url('./assets/crew/background-crew-tablet.jpg')] bg-repeat-y max-lg:h-[130vh]"}>
 <div className='w-[1110px] h-[792px] m-auto pt-12 mt-4 max-lg:w-[688px]'>
 <h2 className='uppercase text-white text-3xl'>  <span className='opacity-20 '>02</span>  Meet your crew</h2>
-<div className='flex space-x-4 mt-[80px] p-10 max-lg:flex-col max-lg:space-y-8 max-lg:items-center'>
-<div className='w-[539px] items-center  space-y-10 grid grid-cols-1 gap-[100px]'>
+<div className='flex space-x-4 mt-[80px] p-10  max-lg:flex-col max-lg:space-y-8 max-lg:items-center'>
+<div className='w-[539px] items-center  space-y-10 grid grid-cols-1 gap-[100px] max-lg:w-[512px]'>
 <div className='text-white w-[445px] h-[254px] space-y-9 max-lg:text-center'>
     <h3 className='text-3xl font-light uppercase opacity-50'>{activeData.job} </h3>
     <h2 className='text-6xl font-light uppercase '>{activeData.Name} </h2>
-    <p className='leading-7 text-blue-200 text-sm'>{activeData.description}</p>
+    <p className='leading-7 text-blue-200 text-[18px]'>{activeData.description}</p>
    </div>
    <ul className='text-white flex justify-start space-x-10 p-7  max-lg:justify-center cursor-pointer'>
    {crewMembers.map((item)=>(<li key={item.Title}><a href={`#${item.Title}`} className={`text-7xl ${active===item.Title?"opacity-100":"opacity-10"}`} onClick={()=>handlechange(`${item.Title}`)}>.</a></li>))}
     </ul>
 </div>
-<div className='w-[539px]'>
-    <img src={activeData.img} alt={activeData.Name} className='w-full h-[640px]'/>
+<div className='w-[539px] max-lg:W-[688px]'>
+    <img src={activeData.img} alt={activeData.Name} className='w-[539px] h-[640px] max-lg:w-[446px] max-lg:h-[560px]'/>
 </div>
 </div>
 </div>
