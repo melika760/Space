730ff1e0a603859225e38acbfdc4fcbd49011fe5
melika760 <melikaaatafazoli@gmail.com>
2024-11-30 16:23:05 +0000

    add more datas to tech

--- src/Components/Constants.js
+++ src/Components/Constants.js
@@ -7,6 +7,8 @@ import Douglas from "../assets/crew/image-douglas-hurley.png"
 import Victor from "../assets/crew/image-victor-glover.png"
 import Anousheh from "../assets/crew/image-anousheh-ansari.png"
 import Launch from "../assets/technology/image-launch-vehicle-landscape.jpg"
+import  capsule from "../assets/technology/image-space-capsule-landscape.jpg"
+import spaceport from "../assets/technology/image-spaceport-landscape.jpg"
 export const datas=[
     {img:moon,
 Title:"Moon",
@@ -68,5 +70,21 @@ export const techs=[
         Name:"LAUNCH VEHICLE",
         job:"THE TERMINOLOGY…",
         description:"A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad! "
+    },
+    {
+        img:spaceport,
+        id:"2",
+        Title:"SPACEPORT",
+        Name:"SPACEPORT",
+        job:"THE TERMINOLOGY…",
+        description:"A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch."
+    },
+    {
+        img:capsule,
+        id:"3",
+        Title:"CAPSULE",
+        Name:"SPACE CAPSULE",
+        job:"THE TERMINOLOGY…",
+        description:"A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained."
     }
 ]
--- src/pages/Technology.js
+++ src/pages/Technology.js
@@ -14,9 +14,7 @@ const Technology = () => {
   {techs.map((item)=>(
     <li className={`border-[1px] border-white rounded-full w-[80px] h-[80px] text-center leading-[80px] text-white text-xl cursor-pointer ${active===item.Title?"bg-white text-black":""}`} key={item.id} onClick={()=>handlechange(item.Title)}><a href={`#${item.Title}`} alt={item.Title} >{item.id}</a></li>
   ))}
-    <li className='border-[1px] border-white rounded-full w-[80px] h-[80px] text-center leading-[80px] text-white text-xl'>1</li>
-    <li className='border-[1px] border-white rounded-full w-[80px] h-[80px] text-center leading-[80px] text-white text-xl'>1</li>
-    <li className='border-[1px] border-white rounded-full w-[80px] h-[80px] text-center leading-[80px] text-white text-xl'>1</li>
+  
   
 </ul>
 <div className='text-white w-[491px] h-[301px] grid gap-[24px] max-lg:text-center'>
@@ -25,7 +23,7 @@ const Technology = () => {
     <p className='leading-7 text-blue-200 text-[18px]'>{activeData.description}</p>
    </div>
 </div>
-<div className='w-[608px]  absolute right-0 '>
+<div className='w-[608px]  absolute right-0 max-lg:hidden'>
   <img src={activeData.img} alt={activeData.Title} className='w-[608px] h-[600px]'/>
 </div>
     </PageLayout>
