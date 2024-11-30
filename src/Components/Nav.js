d2adc04b64804ca95ad5bab9103142c064613b51
melika760 <melikaaatafazoli@gmail.com>
2024-11-30 17:49:10 +0000

    make it responsive

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -6,12 +6,12 @@ const Nav = () => {
   const location=useLocation()
   const isActive=(path)=>location.pathname===path
   return (
- <header className='pt-10 flex justify-center'>
-    <div className='flex justify-stretch items-center  xl:w-[576px] xl:ml-[20px] md:w-[160px] md:justify-center'>
+ <header className='pt-10 flex justify-center '>
+    <div className='flex justify-stretch items-center  xl:w-[576px] xl:ml-[20px] md:w-[160px] md:justify-center '>
     <img src={logo} className='mr-8 ml-8' width={48} height={48} alt='logo'/>
     <img src={line} className='w-[540px] h-[0.15rem] opacity-25 md:hidden xl:block'  alt='line'/>
     </div>
-   <nav className='bg-white bg-opacity-10 h-[96px] xl:w-[946px] max-lg:w-[660px]'>
+   <nav className='bg-white bg-opacity-10 h-[96px] xl:w-[946px] max-lg:w-[660px] max-md:hidden'>
 <ul className='text-white flex justify-center items-center h-full xl:space-x-12  md:space-x-3'>
     <li className='font-light'><Link to={"/"} className={isActive('/')?"border-b-2 p-9":"xl:hover:border-b-2 p-9"}><strong>00</strong> Home</Link></li>
     <li className='font-light'><Link to={"/destination"} className={isActive('/destination')?"border-b-2 p-9":"xl:hover:border-b-2 p-9"}><strong>01</strong> Destination</Link></li>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import PageLayout from '../Components/PageLayout'
+import Nav from '../Components/Nav'
 
 const Home = () => {
   return (
-  <PageLayout bgClass={"xl:bg-[url('./assets/home/background-home-desktop.jpg')] md:bg-[url('./assets/home/background-home-tablet.jpg')]  overflow-hidden"}>
-      <div className='xl:flex xl:justify-between  xl:h-[632px] xl:w-[1110px]  md:h-[928px]  md:flex max-lg:flex-col md:justify-between md:mt-[8rem] md:items-center m-auto items-center max-lg:text-center'>
-        <div className=' text-white w-[540px] md:w-[512px] h-[343px] grid grid-cols-1 gap-5'><p className='uppercase font-light text-blue-200 leading-none'>So you want to travel to </p>
-        <h2 className='font-light text-9xl leading-none'>Space</h2>
+  <section className="xl:bg-[url('./assets/home/background-home-desktop.jpg')] md:bg-[url('./assets/home/background-home-tablet.jpg') bg-[url('./assets/home/background-home-mobile.jpg')]  xl:h-[130vh] md:h-screen bg-cover bg-center xl:bg-repeat-y ">
+    <Nav/>
+      <div className='xl:flex xl:justify-between h-[632px]  xl:h-[632px] xl:w-[1110px]  md:h-[928px]  md:flex max-lg:flex-col md:justify-between md:mt-[8rem] justify-evenly mt-12 xl:mt-0 text-center  m-auto items-center w-[323px]'>
+        <div className=' text-white w-[540px] md:w-[512px] h-[343px] grid grid-cols-1 gap-5 mt-[6rem]'><p className='uppercase font-light text-blue-200 leading-none '>So you want to travel to </p>
+        <h2 className='font-light text-9xl leading-none max-md:text-5xl'>Space</h2>
         <p className=' font-light text-blue-200 leading-6'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience! </p></div>
-        <button className='w-[272px] h-[272px] bg-white rounded-full text-4xl font-light'><Link to={"/destination"}>Explore</Link></button>
+        <button className='w-[272px] h-[272px] bg-white rounded-full text-4xl font-light '><Link to={"/destination"}>Explore</Link></button>
       </div>
-      </PageLayout>
+      </section>
   )
 }
 
