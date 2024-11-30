d77e6e3baf5df3be1b68cdb2fd55fd00843e8a80
melika760 <melikaaatafazoli@gmail.com>
2024-11-30 16:53:17 +0000

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,11 +5,11 @@ import PageLayout from '../Components/PageLayout'
 const Home = () => {
   return (
   <PageLayout bgClass={"xl:bg-[url('./assets/home/background-home-desktop.jpg')] md:bg-[url('./assets/home/background-home-tablet.jpg')]  overflow-hidden"}>
-      <div className='xl:flex xl:justify-between  xl:h-[632px] xl:w-[1110px] md:w-[768px] md:h-[928px]  md:grid max-lg:grid-cols-1 md:gap-20 m-auto items-center max-lg:text-center'>
+      <div className='xl:flex xl:justify-between  xl:h-[632px] xl:w-[1110px]  md:h-[928px]  md:flex max-lg:flex-col md:justify-between md:mt-[8rem] md:items-center m-auto items-center max-lg:text-center'>
         <div className=' text-white w-[540px] md:w-[512px] h-[343px] grid grid-cols-1 gap-5'><p className='uppercase font-light text-blue-200 leading-none'>So you want to travel to </p>
         <h2 className='font-light text-9xl leading-none'>Space</h2>
         <p className=' font-light text-blue-200 leading-6'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience! </p></div>
-        <button className='w-[272px] h-[272px] bg-white rounded-full text-4xl font-light '><Link to={"/destination"}>Explore</Link></button>
+        <button className='w-[272px] h-[272px] bg-white rounded-full text-4xl font-light'><Link to={"/destination"}>Explore</Link></button>
       </div>
       </PageLayout>
   )
