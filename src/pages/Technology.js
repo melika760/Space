7311b8ae3d659b8f3d92abe40127f11181337a04
melika760 <melikaaatafazoli@gmail.com>
2024-12-04 21:46:27 +0000

    finish project

--- src/pages/Technology.js
+++ src/pages/Technology.js
@@ -5,11 +5,11 @@ import useActive from '../Components/use-active'
 const Technology = () => {
   const{active,handlechange,activeData}=useActive("Launch",techs)
   return (
-    <PageLayout space={"justify-between  mt-[80px] max-lg:mt-2"} num={"03"} des={"SPACE LAUNCH 101"} bgClass={"xl:bg-[url('./assets/technology/background-technology-desktop.jpg')] md:bg-[url('./assets/technology/background-technology-tablet.jpg')] bg-[url('./assets/technology/background-technology-mobile.jpg')]  h-[180vh]"} height={"h-[180vh]"}>
-<div className='w-[768px] xl:hidden max-md:w-[372px]'>
+    <PageLayout space={"justify-between  mt-[80px] max-lg:mt-2"} num={"03"} des={"SPACE LAUNCH 101"} bgClass={"xl:bg-[url('./assets/technology/background-technology-desktop.jpg')] md:bg-[url('./assets/technology/background-technology-tablet.jpg')] bg-[url('./assets/technology/background-technology-mobile.jpg')]  h-[180vh] max-xs:h-[220vh]"} height={"h-[180vh]"}>
+<div className='w-[768px] xl:hidden max-md:w-[372px] max-md:p-2'>
   <img src={activeData.img2} alt={activeData.Title} className='w-full h-[600px]'/>
 </div>
-<div className='w-[635px] h-[304px] max-md:w-[372px] max-lg:w-[688px] flex items-center max-md:space-x-0  space-x-8 mt-[100px] relative max-lg:flex-col max-lg:items-center max-lg:space-y-11'>
+<div className='w-[635px] h-[304px] max-md:p-2 max-md:w-[372px] max-lg:w-[688px] flex items-center max-md:space-x-0  space-x-8 mt-[100px] relative max-lg:flex-col max-lg:items-center max-lg:space-y-11'>
 
 <ul className='grid gap-[32px] items-center max-lg:flex'>
   {techs.map((item)=>(
@@ -18,9 +18,9 @@ const Technology = () => {
   
   
 </ul>
-<div className='text-white w-[491px] h-[301px] grid gap-[24px] items-center max-lg:text-center max-md:w-[372px]'>
-    <h3 className='text-3xl font-light uppercase opacity-50'>{activeData.job} </h3>
-    <h2 className='text-6xl font-light uppercase max-md:text-5xl '>{activeData.Name}</h2>
+<div className='text-white w-[491px] h-[301px] grid gap-[24px] items-center max-lg:text-center max-md:w-[372px] max-md:p-2'>
+    <h3 className='text-3xl font-light uppercase opacity-50 max-md:text-xl'>{activeData.job} </h3>
+    <h2 className='text-6xl font-light uppercase max-md:text-4xl '>{activeData.Name}</h2>
     <p className='leading-7 text-blue-200 text-[18px]'>{activeData.description}</p>
    </div>
 </div>
