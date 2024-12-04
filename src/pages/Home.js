ba29f3ca02dc4b43abf06814b0872c82d0c5f609
melika760 <melikaaatafazoli@gmail.com>
2024-12-04 21:40:02 +0000

    change css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
   <section className="xl:bg-[url('./assets/home/background-home-desktop.jpg')] md:bg-[url('./assets/home/background-home-tablet.jpg') bg-[url('./assets/home/background-home-mobile.jpg')]  h-[130vh] bg-cover bg-center bg-repeat-y  ">
     <Nav height={"h-[130vh]"}/>
-      <div className='flex xl:justify-between h-[632px]  max-xs:h-[632px] xl:w-[1110px]  max-md:h-[928px]  md:flex max-lg:flex-col  md:mt-[15rem]   mt-[12rem] justify-between  xl:mt-0 text-center  m-auto items-center w-[323px]'>
+      <div className='flex xl:justify-between h-[632px]  max-xs:h-[632px] xl:w-[1110px]  max-md:h-[928px]  md:flex max-lg:flex-col  md:mt-[15rem] max-xs:mt-[9rem]  mt-[12rem] justify-evenly  xl:mt-0 text-center  m-auto items-center w-[323px]'>
         <div className=' text-white xl:w-[540px] md:w-[512px] h-[343px] grid grid-cols-1 gap-5'><p className='uppercase font-light text-blue-200 leading-none '>So you want to travel to </p>
         <h2 className='font-light text-9xl leading-none max-md:text-5xl'>Space</h2>
         <p className=' font-light text-blue-200 leading-6'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience! </p></div>
--- src/pages/Technology.js
+++ src/pages/Technology.js
@@ -5,22 +5,22 @@ import useActive from '../Components/use-active'
 const Technology = () => {
   const{active,handlechange,activeData}=useActive("Launch",techs)
   return (
-    <PageLayout space={"justify-between mt-[80px] max-lg:mt-2"} num={"03"} des={"SPACE LAUNCH 101"} bgClass={"xl:bg-[url('./assets/technology/background-technology-desktop.jpg')] md:bg-[url('./assets/technology/background-technology-tablet.jpg')] bg-[url('./assets/technology/background-technology-mobile.jpg')]  h-[150vh]"}>
-<div className='w-[768px] xl:hidden'>
+    <PageLayout space={"justify-between  mt-[80px] max-lg:mt-2"} num={"03"} des={"SPACE LAUNCH 101"} bgClass={"xl:bg-[url('./assets/technology/background-technology-desktop.jpg')] md:bg-[url('./assets/technology/background-technology-tablet.jpg')] bg-[url('./assets/technology/background-technology-mobile.jpg')]  h-[180vh]"} height={"h-[180vh]"}>
+<div className='w-[768px] xl:hidden max-md:w-[372px]'>
   <img src={activeData.img2} alt={activeData.Title} className='w-full h-[600px]'/>
 </div>
-<div className='w-[635px] h-[304px] max-lg:w-[688px] flex items-center space-x-8 mt-[100px] relative max-lg:flex-col max-lg:items-center max-lg:space-y-11'>
+<div className='w-[635px] h-[304px] max-md:w-[372px] max-lg:w-[688px] flex items-center max-md:space-x-0  space-x-8 mt-[100px] relative max-lg:flex-col max-lg:items-center max-lg:space-y-11'>
 
-<ul className='grid gap-[32px] max-lg:flex'>
+<ul className='grid gap-[32px] items-center max-lg:flex'>
   {techs.map((item)=>(
-    <li className={`border-[1px] border-white rounded-full w-[80px] h-[80px] text-center leading-[80px] text-white text-xl cursor-pointer ${active===item.Title?"bg-white text-blue-950":""}`} key={item.id} onClick={()=>handlechange(item.Title)}><a href={`#${item.Title}`} alt={item.Title} >{item.id}</a></li>
+    <li className={`border-[1px] border-white rounded-full w-[80px] h-[80px] text-center leading-[80px]  text-xl cursor-pointer ${active===item.Title?"bg-white text-blue-950":"text-white"}`} key={item.id} onClick={()=>handlechange(item.Title)}><a href={`#${item.Title}`} alt={item.Title} >{item.id}</a></li>
   ))}
   
   
 </ul>
-<div className='text-white w-[491px] h-[301px] grid gap-[24px] max-lg:text-center'>
+<div className='text-white w-[491px] h-[301px] grid gap-[24px] items-center max-lg:text-center max-md:w-[372px]'>
     <h3 className='text-3xl font-light uppercase opacity-50'>{activeData.job} </h3>
-    <h2 className='text-6xl font-light uppercase '>{activeData.Name}</h2>
+    <h2 className='text-6xl font-light uppercase max-md:text-5xl '>{activeData.Name}</h2>
     <p className='leading-7 text-blue-200 text-[18px]'>{activeData.description}</p>
    </div>
 </div>
